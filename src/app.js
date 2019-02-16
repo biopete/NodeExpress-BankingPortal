497fb13566da534aae8fbdfe84fb74f94c28ee46
peter <peter@peters-MBP.attlocal.net>
2019-02-15 18:49:31 -0600

    Completed the module

--- src/app.js
+++ src/app.js
@@ -14,11 +14,38 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 */
 
+/* Read Accounts and Users Data*/
+const accountsInit = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'),{encoding: 'utf8'});
+const accounts = JSON.parse(accountsInit);
+console.log(accounts);
+
+const usersInit = fs.readFileSync(path.join(__dirname, 'json', 'users.json'),{encoding: 'utf8'});
+const users = JSON.parse(usersInit);
+console.log(users);
+
+
+
 app.get('/', (req, res) => {
-  res.render('index', {'title': 'Index'});
+  res.render('index', {'title': 'Accounts Summary', 'accounts': accounts});
+  return;
+});
+app.get('/savings', (req, res) => {
+  res.render('account', {'title': 'Savings Summary', 'account': accounts.savings});
+  return;
+});
+app.get('/checking', (req, res) => {
+  res.render('account', {'title': 'Checking Summary', 'account': accounts.checking});
+  return;
+});
+app.get('/credit', (req, res) => {
+  res.render('account', {'title': 'Credit Summary', 'account': accounts.credit});
   return;
 });
 
+app.get('/profile', (req, res) => {
+  res.render('profile', {'title': 'Profile', 'user': users[0]});
+  return;
+});
 app.listen(port, () => {
 console.log(`Listening on ${port}`);
 });
